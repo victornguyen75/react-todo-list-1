0eb84f5fe35e7a34ada652c3b4f2fe90b8e82c7e
Nguyen, Victor <vdn140030@gmail.com>
2020-12-19 15:42:51 -0600

    feat(App.js): save and load items from local storage, so refreshing the screen does not lose your progress

--- src/App.js
+++ src/App.js
@@ -44,6 +44,46 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this),
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this),
+    );
+
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+    for (let key in this.state) {
+      if (localStorage.hasOwnProperty(key)) {
+        let value = localStorage.getItem(key);
+
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // Handle empty strings
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    for (let key in this.state) {
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
   render() {
     return (
       <div className="App">
